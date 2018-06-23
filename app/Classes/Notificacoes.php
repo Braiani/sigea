ef474ac7f1b841a8c647db9ecce24aa14b59ac98
Felipe Braiani <felipe.braianitech@gmail.com>
2018-06-23 12:24:15 -0400

    Tasks Notification implemented

--- app/Classes/Notificacoes.php
+++ app/Classes/Notificacoes.php
@@ -2,6 +2,7 @@
 namespace App\Classes;
 
 use App\Mensagem;
+use App\Task;
 
 class Notificacoes
 {
@@ -15,16 +16,40 @@ class Notificacoes
 
     public function handle($id)
     {
-        if (Mensagem::mailTo($id)->where('read', 0)->count() > 0) {
-            array_push($this->retorno, [
-            'link' => 'sigea.mensagens.index',
-            'mensagem' => 'Você possui mensagens não lidas!'
-        ]);
-            $this->total++;
-        }
+        $this->mensagens($id);
+        $this->tasks($id);
+
         return [
             'total' => $this->total,
             'not_cont' => $this->retorno
         ];
+    }
+
+    public function mensagens($id)
+    {
+        $mensagem = Mensagem::mailTo($id)->get()->filter(function ($value, $key){
+            return $value->read == NULL or $value->read == 0;
+        })->count();
+        if ($mensagem > 0) {
+            array_push($this->retorno, [
+                'link' => 'sigea.mensagens.index',
+                'mensagem' => "Você possui {$mensagem} mensagens não lidas!"
+            ]);
+            $this->total += $mensagem;
+        }
+    }
+
+    public function tasks($id)
+    {
+        $tasks = Task::toUser($id)->get()->filter(function ($value, $key){
+            return $value->completed == NULL or $value->completed == 0;
+        })->count();
+        if ($tasks > 0) {
+            array_push($this->retorno, [
+                'link' => 'sigea.dashboard',
+                'mensagem' => "Você possui {$tasks} tarefas não concluídas!"
+            ]);
+            $this->total += $tasks;
+        }
     }
 }
