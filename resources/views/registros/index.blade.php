ab749aafd1a13297b88a9b8f4f3e8ee07b42d4de
Felipe Braiani <felipe.braianitech@gmail.com>
2018-07-10 01:14:33 -0400

    Improved the Student selection using Bootstrap-Select and Ajax

--- resources/views/registros/index.blade.php
+++ resources/views/registros/index.blade.php
@@ -2,16 +2,17 @@
 
 @section('title', 'Registro de rematrícula')
 
+@push('css')
+<link href="{{asset('/css/ajax-bootstrap-select.css')}}" rel="stylesheet" />
+@endpush
+
 @section('content')
 <div class="row">
     <div class="col-sm-10 col-md-8">
         <h3>Por favor, Selecione o estudante abaixo.</h3>
         <div class="form-group">
-            <select id="aluno" name="aluno" class="form-control selectpicker" data-style="btn-info">
-                <option value=''>-- Selecione o aluno --</option>
-                @foreach ($alunos as $aluno)
-                    <option value="{{$aluno->id}}">{{$aluno->nome}} - {{$aluno->curso->nome}} - {{$aluno->matricula}}</option>
-                @endforeach
+            <select id="aluno" name="aluno" class="form-control selectpicker" data-style="btn-info"
+                data-live-search="true">
             </select>
         </div>
         <a class="btn btn-info btn-lg" id="btnRegistro" target="" href="#">Registrar</a>
@@ -20,8 +21,29 @@
 @endsection
 
 @push('script')
+<script src="{{asset('/js/plugins/ajax-bootstrap-select.js')}}" type="text/javascript"></script>
+<script src="{{asset('/js/plugins/i18n/ajax-bootstrap-select.pt-BR.js')}}" type="text/javascript"></script>
     <script>
         $(document).ready(function() {
+            var options = {
+                ajax          : {
+                    headers   : {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    url     : '{{ route("sigea.registros.getAlunos") }}',
+                    type    : 'POST',
+                    dataType: 'json',
+                    data    : {
+                        q: '@{{{q}}}'
+                    }
+                },
+                locale        : {
+                    emptyTitle: 'Digite para procurar...'
+                },
+                log           : 3,
+            };
+            $('.selectpicker').selectpicker().ajaxSelectPicker(options);
+            $('select').trigger('change');
             $('#aluno').on('change', function(){
                 if($(this).val() !== ""){
                     $('#btnRegistro').attr('href', 'registros/' + $(this).val());
